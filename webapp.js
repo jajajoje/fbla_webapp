0865b75f7bfea262e83ad8dff5ea816e50c84932
jajajoje <jajajoje1@gmail.com>
2023-09-06 09:15:07 -0500

    major fornt end additons

---
+++ webapp.js
@@ -0,0 +1,76 @@
+var exampleData = [
+    // [Company Name, Resources Provided, Location, Founded Year, Website]
+    ["Company A", "Software Development, IT Consulting", "New York, USA", 2000, "www.companyA.com"],
+    ["Company B", "Manufacturing, Supply Chain Management", "Los Angeles, USA", 1995, "www.companyB.com"],
+    ["Company C", "Biotechnology, Research Services", "San Francisco, USA", 2010, "www.companyC.com"],
+    ["Company D", "Financial Services, Investment Banking", "London, UK", 1985, "www.companyD.co.uk"],
+    ["Company E", "E-commerce, Online Retail", "Seattle, USA", 2008, "www.companyE.com"],
+    ["Company F", "Healthcare, Medical Devices", "Boston, USA", 1998, "www.companyF.com"],
+    ["Company G", "Green Energy, Solar Power", "Berlin, Germany", 2012, "www.companyG.de"],
+    ["Company H", "Telecommunications, Network Solutions", "Tokyo, Japan", 1990, "www.companyH.co.jp"],
+    ["Company I", "Automotive, Car Manufacturing", "Detroit, USA", 1970, "www.companyI.com"],
+    ["Company J", "Aerospace, Space Exploration", "Houston, USA", 1965, "www.companyJ.com"],
+    ["Company K", "Food Processing, Agriculture", "Paris, France", 2005, "www.companyK.fr"],
+    ["Company L", "Fashion, Apparel Design", "Milan, Italy", 1992, "www.companyL.it"],
+    ["Company M", "Entertainment, Media Production", "Los Angeles, USA", 1980, "www.companyM.com"],
+    ["Company N", "Education, E-learning", "Toronto, Canada", 2015, "www.companyN.ca"],
+    ["Company O", "Transportation, Logistics", "Shanghai, China", 2003, "www.companyO.cn"],
+    ["Company P", "Real Estate, Property Development", "Dubai, UAE", 1999, "www.companyP.ae"],
+    ["Company Q", "Pharmaceuticals, Drug Research", "Mumbai, India", 2011, "www.companyQ.in"],
+    ["Company R", "Consumer Electronics, Gadgets", "Seoul, South Korea", 2007, "www.companyR.kr"],
+    ["Company S", "Hospitality, Hotel Management", "Las Vegas, USA", 1975, "www.companyS.com"],
+    ["Company T", "Environmental Services, Recycling", "Oslo, Norway", 2013, "www.companyT.no"],
+    ["Company U", "Legal Services, Law Consulting", "New York, USA", 1997, "www.companyU.com"],
+    ["Company V", "Pharmaceuticals, Drug Manufacturing", "Berlin, Germany", 1988, "www.companyV.de"],
+    ["Company W", "Advertising, Marketing", "London, UK", 2001, "www.companyW.co.uk"],
+    ["Company X", "Software as a Service (SaaS), Cloud Computing", "San Francisco, USA", 2010, "www.companyX.com"],
+    ["Company Y", "Renewable Energy, Wind Power", "Stockholm, Sweden", 2006, "www.companyY.se"],
+    ["Company Z", "Consulting, Business Advisory", "Sydney, Australia", 1994, "www.companyZ.com"],
+]
+
+var buttons = []
+
+createList(exampleData);
+
+
+function createList(list){
+    for(var i = 0; i < exampleData.length; i++){
+        buttons[i] = document.createElement("button")
+        var container = document.getElementById("spreadsheet")
+        container.appendChild(buttons[i])
+
+        buttons[i].innerHTML = exampleData[i]
+        buttons[i].style.backgroundColor = "#1d52bc"
+        buttons[i].style.width = "90%"
+        buttons[i].style.height = "50px"
+        buttons[i].style.position = "absolute"
+        buttons[i].style.marginTop = "-25px"
+        buttons[i].style.left = "5%"
+        buttons[i].style.top = 135+60*i+"px"
+    }
+    document.getElementById("spreadsheet").style.height = exampleData.length*61;
+    // container.style.height = exampleData.length*75;
+}
+
+function removeListItem (){
+
+};
+
+function addListItem (){
+
+};
+
+function buttonClicked(){
+    // var element = document.getElementById("infoId")
+    element !== null ? document.getElementById("infoId").remove() : console.log(false);
+    infoPage = document.createElement("div");
+    var container = document.getElementById("spreadsheet");
+    infoPage.setAttribute("id", "infoId");
+    container.appendChild(infoPage);
+};
+
+
+
+buttons[0].addEventListener('click', () => {
+    buttonClicked(0);
+});
